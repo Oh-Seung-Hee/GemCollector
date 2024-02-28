296a06644a9072b37a8fc880f7e652e47ef47d6f
minsang95 <154496356+minsang95@users.noreply.github.com>
2024-02-29 00:47:54 +0900

    [Feat] Enemy

--- Assets/JMS/ScriptableObjects/Scripts/PlayerSO.cs
+++ Assets/JMS/ScriptableObjects/Scripts/PlayerSO.cs
@@ -7,4 +7,4 @@ public class PlayerSO : ScriptableObject
     [field: SerializeField] public PlayerGroundData GroundedData { get; private set; }
     [field: SerializeField] public PlayerAirData AirData { get; private set; }
     [field: SerializeField] public PlayerAttackData AttakData { get; private set; }
-}
+}   
