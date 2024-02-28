3aba4559fb4216c19089f180d080e87aad9d175e
minsang95 <154496356+minsang95@users.noreply.github.com>
2024-02-28 18:37:33 +0900

    [Fix] 카메라 스크립트 수정

--- Assets/JMS/Scripts/CameraTransform.cs
+++ Assets/JMS/Scripts/CameraTransform.cs
@@ -4,9 +4,15 @@ using UnityEngine;
 
 public class CameraTransform : MonoBehaviour
 {
-    [SerializeField] private Transform Player;
+    private Transform Player;
     private void Update()
     {
-        transform.position = new Vector3(Player.position.x, Player.position.y + 7f, Player.position.z - 7f);
+        if (Player == null)
+        {
+            if (GameObject.FindWithTag("Player") != null)
+                Player = GameObject.FindWithTag("Player").transform;
+        }
+        if(Player != null)
+            transform.position = new Vector3(Player.position.x, Player.position.y + 7f, Player.position.z - 7f);
     }
 }
